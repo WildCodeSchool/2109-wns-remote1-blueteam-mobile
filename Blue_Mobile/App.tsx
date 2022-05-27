8b3d82e25d1bdba07992e938a2d1232897ef0161
benjamin <benjamin.d@make-it.fr>
2022-05-27 14:23:53 +0200

    add apollo client provider & import uri constant

--- Blue_Mobile/App.tsx
+++ Blue_Mobile/App.tsx
@@ -3,6 +3,24 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
+
+import uri from './constants/Uri';
+
+const link = createHttpLink({
+  uri,
+  credentials: 'include',
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -10,12 +28,14 @@ export default function App() {
 
   if (!isLoadingComplete) {
     return null;
-  } else {
-    return (
+  }
+  
+  return (
+    <ApolloProvider client={client}>
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
-    );
-  }
+    </ApolloProvider>
+  );
 }
