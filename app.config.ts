8ee41e96fb40adf56e39f4a677820973003ba5c9
benjamin <benjamin.d@make-it.fr>
2022-06-22 15:58:04 +0200

    fix config file to use .env values

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,39 @@
 import 'dotenv/config';
 
-import { ExpoConfig, ConfigContext } from '@expo/config';
-
-export default ({ config }: ConfigContext): ExpoConfig => ({
-    ...config,
-
-    extra: {
-        apiUrl: process.env.API_URL ?? '/graphql',
+export default {
+  expo: {
+    name: "application",
+    slug: "application",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
     },
-});
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
+    },
+    web: {
+      favicon: "./assets/images/favicon.png"
+    },
+    extra: {
+      apiUrl: process.env.API_URL
+    }
+  }
+};
